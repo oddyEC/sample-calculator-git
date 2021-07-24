eac44dea677751a6ff91f97b7080dbf68f67ac81
diego <diego.marquez@epn.edu.ec>
2021-07-24 11:23:14 -0500

    Adding calculator execute class. Adding calculator java archiver JAR.

---
+++ src/main/java/ec/edu/epn/git/calculator/CalculatorExecute.java
@@ -0,0 +1,15 @@
+package ec.edu.epn.git.calculator;
+
+public class CalculatorExecute {
+    public static void main(String[] args) {
+        System.out.println("== Calculator Execute ==");
+
+        Calculator c = new Calculator();
+        int addition = c.addition(6,9);
+        System.out.println("c.addition(6,9) = "+ addition);
+
+        int substraction = c.substraction(9,3);
+        System.out.println("c.substraction(9,3) = "+ substraction);
+
+    }
+}
